ea6946acd5f0fdce3ecf09bedb6ad3eb52f85c87
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-02 01:35:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,16 @@
 def unsafe?(speed)
   if speed < 40 || speed > 60
-    unsafe
+    "unsafe"
   else
+    "safe"
+  end 
 
 end
 
 
 
 def not_safe?(speed)
-  
+  speed < 40 || speed > 60 ? "unsafe" : "safe"
 	
 end
 	
